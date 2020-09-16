beeab359917910a407fb71874a1cbae911395db8
Pyan <62762243+PY44N@users.noreply.github.com>
2020-09-16 12:28:55 -0400

    Update Base64Decode.cs

--- Multitool/Base64Decode.cs
+++ Multitool/Base64Decode.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -19,12 +19,14 @@ namespace Multitool
 
         private void Base64Decode_Load(object sender, EventArgs e)
         {
+            //Sets form title and sets theme
             Text = Global.title + " | Base64 Decode";
             Theme.Run(this);
         }
 
         private void Decode_Click(object sender, EventArgs e)
         {
+            //Decodes Base64 and Sets output text to decoded base64
             Out.Text = Encoding.ASCII.GetString(Convert.FromBase64String(ToDecode.Text));
         }
     }
