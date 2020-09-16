0848fbdbb169d4be8b34c34b0682377d231259f4
Pyan <62762243+PY44N@users.noreply.github.com>
2020-09-16 12:36:50 -0400

    Update IpInfo.cs

--- Multitool/IpInfo.cs
+++ Multitool/IpInfo.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -24,22 +24,28 @@ namespace Multitool
 
         private void IPInfo_Load(object sender, EventArgs e)
         {
+            //Sets form title
             Text = Global.title + " | IP Info";
+            //Sets theme
             Theme.Run(this);
 
+            //Grabs IP
             WebClient wc = new WebClient();
             Info = wc.DownloadString("http://ip-api.com/json/");
             wc.Dispose();
             Json = JsonConvert.DeserializeObject(Info);
+            //Sets textbox to IP
             IP.Text = Json.query;
         }
 
         private void Search_Click(object sender, EventArgs e)
         {
+            //Grabs IP Info
             WebClient wc = new WebClient();
             Info = wc.DownloadString("http://ip-api.com/json/" + IP.Text);
             wc.Dispose();
             Json = JsonConvert.DeserializeObject(Info);
+            //Sets output to ip info
             Out.Text = "IP: " + Json.query + "\nCountry: " + Json.country + "\nRegion: " + Json.regionName + "\nCity: " + Json.city + "\nZip: " + Json.zip + "\nTimezone: " + Json.timezone + "\nISP: " + Json.isp;
         }
     }
