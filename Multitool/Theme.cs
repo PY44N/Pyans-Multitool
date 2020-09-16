ca0a41297eda8f02bb12781506ded1c3f90d3dd3
Pyan <62762243+PY44N@users.noreply.github.com>
2020-09-16 12:27:13 -0400

    Update Theme.cs

--- Multitool/Theme.cs
+++ Multitool/Theme.cs
@@ -1,6 +1,7 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,7 @@ namespace Multitool
 {
     class Theme
     {
+        //Defines Colors for each element
         public static Color Background = Color.FromArgb(255, 255, 255);
         public static Color Text = Color.FromArgb(0, 0, 0);
         public static Color Button = Color.FromArgb(255, 255, 255);
@@ -17,8 +19,10 @@ namespace Multitool
 
         public static void Run(Form form)
         {
+            //Sets form background color
             form.BackColor = Background;
 
+            //Sets each button color
             foreach (var button in form.Controls.OfType<Button>())
             {
                 button.BackColor = Button;
@@ -26,11 +30,13 @@ namespace Multitool
                 button.FlatStyle = FlatStyle.Popup;
             }
 
+            //Sets each label color
             foreach (var label in form.Controls.OfType<Label>())
             {
                 label.ForeColor = Text;
             }
 
+            //Sets each link color
             foreach (var link in form.Controls.OfType<LinkLabel>())
             {
                 link.LinkColor = Link;
